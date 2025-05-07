fef803edc2b478a9bd230b27d6c2d758bf6fb449
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-05-07 19:41:37 +0700

    fix:dashboard.blade, and login.blade

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,10 +28,12 @@ public function login(Request $request)
 
 
 
-public function logout(){
+public function logout(Request $request){
 
 
     Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
     return redirect()->route('login')->with('success', 'Logout successful!');
 }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,23 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1>Login</h1>
-
-
-
+   
+    <div class="flex h-screen bg-indigo-700">
+        <div class="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">  
+            <header>
+                <img class="w-20 mx-auto mb-5" src="https://img.icons8.com/fluent/344/year-of-tiger.png" />
+              </header> 
+        
 
     <form action="{{route('proses.login')}}" method="POST">
      @csrf
-        <label for="email">Masukkan Email</label>
-        <br></br>
-        <input type="email" name="email" id="email">
-        <br></br>
-        <label for="password">Masukkan Password</label>
-        <input type="password" name="password" id="password">
-        <br></br>
-        <button type="submit">Login</button>
+     <div>
+        <label class="block mb-2 text-indigo-500" for="email">Masukkan Email</label>
+      
+        <input  class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="email" name="email" id="email">
+     </div>
+     <div>
+        <label class="block mb-2 text-indigo-500" for="password">Masukkan Password</label>
+        <input class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="password" name="password" id="password">
+     </div>
+        <div>          
+        <input class="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded" type="submit">
+      </div>   
     </form>
+
+    </div>
 </body>
 </html>
